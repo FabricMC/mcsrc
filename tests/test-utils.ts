22a91930407632af09db6b4cac05033d51b6ee8d
Dimas Firmansyah <deirn@bai.lol>
2026-02-14 11:42:26 +0700

    Revert "fix e2e"
    This reverts commit b4028ac07bc15501c0ddd528c4c08016c81eaf6c.

--- tests/test-utils.ts
+++ tests/test-utils.ts
@@ -1,8 +1,10 @@
 import { expect, Page } from '@playwright/test';
 
 export async function waitForDecompiledContent(page: Page, expectedText: string) {
-    const decompiling = page.getByText('Decompiling...');
-    await decompiling.waitFor({ state: 'hidden', timeout: 30000 });
+    await expect(async () => {
+        const decompiling = page.getByText('Decompiling...');
+        await expect(decompiling).toBeHidden();
+    }).toPass({ timeout: 30000 });
 
     const editor = page.getByRole("code").nth(0);
     await expect(editor).toContainText(expectedText, { timeout: 30000 });
@@ -14,13 +16,13 @@ export async function setupTest(page: Page) {
     });
 
     await page.goto('/');
-
-    const downloading = page.getByText('Downloading Minecraft Jar');
-    await downloading.waitFor();
-    await downloading.waitFor({ state: 'hidden', timeout: 300000 });
 }
 
 export async function waitForIndexing(page: Page) {
+    const downloading = page.getByText('Downloading Minecraft Jar');
+    await downloading.waitFor();
+    await downloading.waitFor({ state: 'hidden', timeout: 300000 });
+
     const indexing = page.getByText('Indexing Minecraft Jar');
     await indexing.waitFor();
     await indexing.waitFor({ state: 'hidden' });
