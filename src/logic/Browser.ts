94053511a2a798efa5e62a7e0ecdc27fa7b4ae9d
modmuss50 <modmuss50@gmail.com>
2026-04-17 00:00:15 +0100

    First pass on light mode

--- src/logic/Browser.ts
+++ src/logic/Browser.ts
@@ -1,8 +1,16 @@
-import { distinctUntilChanged, fromEvent, map, startWith, throttleTime } from "rxjs";
+import { distinctUntilChanged, fromEvent, map, merge, startWith, throttleTime } from "rxjs";
 
 export const isThin = fromEvent(window, 'resize').pipe(
     startWith(null),
     map(() => window.innerWidth < 800),
     throttleTime(50),
+    distinctUntilChanged()
+);
+
+const darkModeQuery = window.matchMedia('(prefers-color-scheme: dark)');
+export const isDarkMode = merge(
+    fromEvent<MediaQueryListEvent>(darkModeQuery, 'change').pipe(map(e => e.matches)),
+).pipe(
+    startWith(darkModeQuery.matches),
     distinctUntilChanged()
 );
