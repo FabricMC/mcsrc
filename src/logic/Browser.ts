43a07c21e2bfa3db1937b98941c4c68fa3e39934
modmuss <modmuss50@gmail.com>
2026-04-20 14:22:20 -0700

    Light mode (#115)
    * First pass on light mode
    
    * Light icons
    
    * Fix light mode toggle
    
    Co-authored-by: steveh259 <27025075+steveh259@users.noreply.github.com>
    
    ---------
    
    Co-authored-by: steveh259 <27025075+steveh259@users.noreply.github.com>

--- src/logic/Browser.ts
+++ src/logic/Browser.ts
@@ -1,8 +1,21 @@
-import { distinctUntilChanged, fromEvent, map, startWith, throttleTime } from "rxjs";
+import { distinctUntilChanged, fromEvent, map, merge, Observable, startWith, throttleTime } from "rxjs";
 
 export const isThin = fromEvent(window, 'resize').pipe(
     startWith(null),
     map(() => window.innerWidth < 800),
     throttleTime(50),
+    distinctUntilChanged()
+);
+
+const darkModeQuery = window.matchMedia('(prefers-color-scheme: dark)');
+export const isDarkMode = new Observable(subscriber => {
+    const emit = () => subscriber.next(darkModeQuery.matches);
+    emit(); // emit initial value
+
+    darkModeQuery.addEventListener("change", emit);
+    return () => {
+        darkModeQuery.removeEventListener("change", emit);
+    };
+}).pipe(
     distinctUntilChanged()
 );
