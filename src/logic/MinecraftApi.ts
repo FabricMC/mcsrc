070cad8c9e3f1573691c295c2735436f401346dd
CelDaemon <celdaemon@voidgroup.net>
2026-06-27 22:46:03 +0200

    Return VersionListEntry[] instead of VersionsList

--- src/logic/MinecraftApi.ts
+++ src/logic/MinecraftApi.ts
@@ -8,7 +8,7 @@ const CACHE_NAME = 'mcsrc-v1';
 const VERSIONS_URL = "https://piston-meta.mojang.com/mc/game/version_manifest_v2.json";
 
 interface VersionsList {
-    versions: VersionListEntry[];
+    versions: VersionListEntry[]
 }
 
 interface VersionListEntry {
@@ -50,7 +50,6 @@ export interface MinecraftJarMetadata {
 export const minecraftVersions = agreedEula.observable.pipe(
     filter(agreed => agreed),
     switchMap(() => from(fetchVersions())),
-    map(versionsList => versionsList.versions),
     tap(versions => {
         // On inital load, if we dont have a version selected or the selected version is not valid, default to the latest version.
         const currentVersion = selectedMinecraftVersion.value;
@@ -103,15 +102,13 @@ async function getJson<T>(url: string): Promise<T> {
     return response.json();
 }
 
-async function fetchVersions(): Promise<VersionsList> {
+async function fetchVersions(): Promise<VersionListEntry[]> {
     const mojang = await getJson<VersionsList>(VERSIONS_URL);
-    const filteredMojangVersions = mojang.filter(isSupported);
+    const filteredMojangVersions = mojang.versions.filter(isSupported);
     const versions = filteredMojangVersions
-        .concat(EXPERIMENTAL_VERSIONS.versions)
+        .concat(EXPERIMENTAL_VERSIONS)
         .sort((a, b) => b.releaseTime.localeCompare(a.releaseTime));
-    return {
-        versions: versions
-    };
+    return versions;
 }
 
 export function isUnobfuscated(version: VersionListEntry): boolean {
@@ -274,95 +271,93 @@ async function prepareMinecraftJarBlob(
 }
 
 // Hardcode as these are never going to change.
-const EXPERIMENTAL_VERSIONS: VersionsList = {
-    versions: [
-        {
-            id: "25w45a_unobfuscated",
-            type: "unobfuscated",
-            url: "https://maven.fabricmc.net/net/minecraft/25w45a_unobfuscated.json",
-            time: "2025-11-04T14:07:08+00:00",
-            releaseTime: "2025-11-04T14:07:08+00:00",
-            sha1: "7a3c149f148b6aa5ac3af48c4f701adea7e5b615",
-        },
-        {
-            id: "25w46a_unobfuscated",
-            type: "unobfuscated",
-            url: "https://maven.fabricmc.net/net/minecraft/25w46a_unobfuscated.json",
-            time: "2025-11-11T13:20:54+00:00",
-            releaseTime: "2025-11-11T13:20:54+00:00",
-            sha1: "314ade2afeada364047798e163ef8e82427c69e1",
-        },
-        {
-            id: "1.21.11-pre1_unobfuscated",
-            type: "unobfuscated",
-            url: "https://maven.fabricmc.net/net/minecraft/1_21_11-pre1_unobfuscated.json",
-            time: "2025-11-19T08:30:46+00:00",
-            releaseTime: "2025-11-19T08:30:46+00:00",
-            sha1: "9c267f8dda2728bae55201a753cdd07b584709f1",
-        },
-        {
-            id: "1.21.11-pre2_unobfuscated",
-            type: "unobfuscated",
-            url: "https://maven.fabricmc.net/net/minecraft/1_21_11-pre2_unobfuscated.json",
-            time: "2025-11-21T12:07:21+00:00",
-            releaseTime: "2025-11-21T12:07:21+00:00",
-            sha1: "2955ce0af0512fdfe53ff0740b017344acf6f397",
-        },
-        {
-            id: "1.21.11-pre3_unobfuscated",
-            type: "unobfuscated",
-            url: "https://maven.fabricmc.net/net/minecraft/1_21_11-pre3_unobfuscated.json",
-            time: "2025-11-25T14:14:30+00:00",
-            releaseTime: "2025-11-25T14:14:30+00:00",
-            sha1: "579bf3428f72b5ea04883d202e4831bfdcb2aa8d",
-        },
-        {
-            id: "1.21.11-pre4_unobfuscated",
-            type: "unobfuscated",
-            url: "https://maven.fabricmc.net/net/minecraft/1_21_11-pre4_unobfuscated.json",
-            time: "2025-12-01T13:40:12+00:00",
-            releaseTime: "2025-12-01T13:40:12+00:00",
-            sha1: "410ce37a2506adcfd54ef7d89168cfbe89cac4cb",
-        },
-        {
-            id: "1.21.11-pre5_unobfuscated",
-            type: "unobfuscated",
-            url: "https://maven.fabricmc.net/net/minecraft/1_21_11-pre5_unobfuscated.json",
-            time: "2025-12-03T13:34:06+00:00",
-            releaseTime: "2025-12-03T13:34:06+00:00",
-            sha1: "1028441ca6d288bbf2103e773196bf524f7260fd",
-        },
-        {
-            id: "1.21.11-rc1_unobfuscated",
-            type: "unobfuscated",
-            url: "https://maven.fabricmc.net/net/minecraft/1_21_11-rc1_unobfuscated.json",
-            time: "2025-12-04T15:56:55+00:00",
-            releaseTime: "2025-12-04T15:56:55+00:00",
-            sha1: "5d3ee0ef1f0251cf7e073354ca9e085a884a643d",
-        },
-        {
-            id: "1.21.11-rc2_unobfuscated",
-            type: "unobfuscated",
-            url: "https://maven.fabricmc.net/net/minecraft/1_21_11-rc2_unobfuscated.json",
-            time: "2025-12-05T11:57:45+00:00",
-            releaseTime: "2025-12-05T11:57:45+00:00",
-            sha1: "9282a3fb154d2a425086c62c11827281308bf93b",
-        },
-        {
-            id: "1.21.11-rc3_unobfuscated",
-            type: "unobfuscated",
-            url: "https://maven.fabricmc.net/net/minecraft/1_21_11-rc3_unobfuscated.json",
-            time: "2025-12-08T13:59:34+00:00",
-            releaseTime: "2025-12-08T13:59:34+00:00",
-            sha1: "ce3f7ac6d0e9d23ea4e5f0354b91ff15039d9931",
-        },
-        {
-            id: "1.21.11_unobfuscated",
-            type: "unobfuscated",
-            url: "https://maven.fabricmc.net/net/minecraft/1_21_11_unobfuscated.json",
-            time: "2025-12-09T12:43:15+00:00",
-            releaseTime: "2025-12-09T12:43:15+00:00",
-            sha1: "327be7759157b04495c591dbb721875e341877af",
-        }
-    ]
-};
+const EXPERIMENTAL_VERSIONS: VersionListEntry[] = [
+    {
+        id: "25w45a_unobfuscated",
+        type: "unobfuscated",
+        url: "https://maven.fabricmc.net/net/minecraft/25w45a_unobfuscated.json",
+        time: "2025-11-04T14:07:08+00:00",
+        releaseTime: "2025-11-04T14:07:08+00:00",
+        sha1: "7a3c149f148b6aa5ac3af48c4f701adea7e5b615",
+    },
+    {
+        id: "25w46a_unobfuscated",
+        type: "unobfuscated",
+        url: "https://maven.fabricmc.net/net/minecraft/25w46a_unobfuscated.json",
+        time: "2025-11-11T13:20:54+00:00",
+        releaseTime: "2025-11-11T13:20:54+00:00",
+        sha1: "314ade2afeada364047798e163ef8e82427c69e1",
+    },
+    {
+        id: "1.21.11-pre1_unobfuscated",
+        type: "unobfuscated",
+        url: "https://maven.fabricmc.net/net/minecraft/1_21_11-pre1_unobfuscated.json",
+        time: "2025-11-19T08:30:46+00:00",
+        releaseTime: "2025-11-19T08:30:46+00:00",
+        sha1: "9c267f8dda2728bae55201a753cdd07b584709f1",
+    },
+    {
+        id: "1.21.11-pre2_unobfuscated",
+        type: "unobfuscated",
+        url: "https://maven.fabricmc.net/net/minecraft/1_21_11-pre2_unobfuscated.json",
+        time: "2025-11-21T12:07:21+00:00",
+        releaseTime: "2025-11-21T12:07:21+00:00",
+        sha1: "2955ce0af0512fdfe53ff0740b017344acf6f397",
+    },
+    {
+        id: "1.21.11-pre3_unobfuscated",
+        type: "unobfuscated",
+        url: "https://maven.fabricmc.net/net/minecraft/1_21_11-pre3_unobfuscated.json",
+        time: "2025-11-25T14:14:30+00:00",
+        releaseTime: "2025-11-25T14:14:30+00:00",
+        sha1: "579bf3428f72b5ea04883d202e4831bfdcb2aa8d",
+    },
+    {
+        id: "1.21.11-pre4_unobfuscated",
+        type: "unobfuscated",
+        url: "https://maven.fabricmc.net/net/minecraft/1_21_11-pre4_unobfuscated.json",
+        time: "2025-12-01T13:40:12+00:00",
+        releaseTime: "2025-12-01T13:40:12+00:00",
+        sha1: "410ce37a2506adcfd54ef7d89168cfbe89cac4cb",
+    },
+    {
+        id: "1.21.11-pre5_unobfuscated",
+        type: "unobfuscated",
+        url: "https://maven.fabricmc.net/net/minecraft/1_21_11-pre5_unobfuscated.json",
+        time: "2025-12-03T13:34:06+00:00",
+        releaseTime: "2025-12-03T13:34:06+00:00",
+        sha1: "1028441ca6d288bbf2103e773196bf524f7260fd",
+    },
+    {
+        id: "1.21.11-rc1_unobfuscated",
+        type: "unobfuscated",
+        url: "https://maven.fabricmc.net/net/minecraft/1_21_11-rc1_unobfuscated.json",
+        time: "2025-12-04T15:56:55+00:00",
+        releaseTime: "2025-12-04T15:56:55+00:00",
+        sha1: "5d3ee0ef1f0251cf7e073354ca9e085a884a643d",
+    },
+    {
+        id: "1.21.11-rc2_unobfuscated",
+        type: "unobfuscated",
+        url: "https://maven.fabricmc.net/net/minecraft/1_21_11-rc2_unobfuscated.json",
+        time: "2025-12-05T11:57:45+00:00",
+        releaseTime: "2025-12-05T11:57:45+00:00",
+        sha1: "9282a3fb154d2a425086c62c11827281308bf93b",
+    },
+    {
+        id: "1.21.11-rc3_unobfuscated",
+        type: "unobfuscated",
+        url: "https://maven.fabricmc.net/net/minecraft/1_21_11-rc3_unobfuscated.json",
+        time: "2025-12-08T13:59:34+00:00",
+        releaseTime: "2025-12-08T13:59:34+00:00",
+        sha1: "ce3f7ac6d0e9d23ea4e5f0354b91ff15039d9931",
+    },
+    {
+        id: "1.21.11_unobfuscated",
+        type: "unobfuscated",
+        url: "https://maven.fabricmc.net/net/minecraft/1_21_11_unobfuscated.json",
+        time: "2025-12-09T12:43:15+00:00",
+        releaseTime: "2025-12-09T12:43:15+00:00",
+        sha1: "327be7759157b04495c591dbb721875e341877af",
+    }
+];
